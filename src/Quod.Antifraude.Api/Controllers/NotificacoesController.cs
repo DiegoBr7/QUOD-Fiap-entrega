c34e2303ed0c81819d1e9fb303735b1f7a34c53b
Paulo Caue Kruger Costa <paulo.krg.costa@gmail.com>
2025-05-13 23:45:43 -0300

    feat: Endpoint para simular notificação de fraude

---
+++ src/Quod.Antifraude.Api/Controllers/NotificacoesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quod.Antifraude.Core.Models;
+
+namespace Quod.Antifraude.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificacoesController : ControllerBase
+    {
+        [HttpPost("fraude")]
+        public IActionResult ReceberFraude([FromBody] RegistroValidacao registro)
+        {
+            // Simule o processamento da notificação
+            return Ok(new { mensagem = "Notificação de fraude recebida com sucesso!" });
+        }
+    }
+}
